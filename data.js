adae414a325b5229e7050e785f2e39e945e9a086
Michael <mdbridgeman@gmail.com>
2024-01-07 20:37:08 -0800

    added the payment form

--- data.js
+++ data.js
@@ -1,24 +1,52 @@
 const menuArray = [
     {
-        name: "Pizza",
-        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        name: "Breakfast",
+        ingredients: ["quiche, biscuits, honey nut cake, and coffee"],
         id: 0,
-        price: 14,
-        emoji: "🍕"
+        price: 25,
+        emoji: "🥧"
     },
     {
-        name: "Hamburger",
-        ingredients: ["beef", "cheese", "lettuce"],
-        price: 12,
-        emoji: "🍔",
+        name: "Second Breakfast",
+        ingredients: ["toast, pastries, bacon, apple, and coffee"],
+        price: 15,
+        emoji: "🥓",
         id: 1
     },
         {
-        name: "Beer",
-        ingredients: ["grain, hops, yeast, water"],
-        price: 12,
-        emoji: "🍺",
+        name: "Elevenses",
+        ingredients: ["lembas bread, strawberry shortcake, muffins, and green tea"],
+        price: 10,
+        emoji: "🍓",
         id: 2
+    },
+    {
+        name: "Luncheon",
+        ingredients: ["roast mutton, scotch eggs, mushrooms, and beer"],
+        price: 20,
+        emoji: "🍺",
+        id: 3
+    },
+    {
+        name: "Afternoon Tea",
+        ingredients: ["ring cookies, pie, tarts, and green tea"],
+        price: 12,
+        emoji: "🍪",
+        id: 4
+    },
+    {
+        name: "Dinner",
+        ingredients: ["steak, roasted vegetables, potatoes, beer"],
+        price: 25,
+        emoji: "🥔",
+        id: 5
+    },
+    {
+        name: "Supper",
+        ingredients: ["soup, salad, charcuterie board, and beer"],
+        price: 60,
+        emoji: "🥗",
+        id: 6
     }
 ]
 
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import menuArray from "./data.js";
 
+const headerEl = document.getElementById("header");
 const foodItem = document.getElementById("menu-container");
 const orderEl = document.getElementById("order-container");
 const checkOutEl = document.getElementById("check-out-container");
@@ -12,8 +13,7 @@ function renderMenu() {
     .map((food) => {
       let quantity =
         cartArray.find((cart) => cart.id === `${food.name}-quantity`) || [];
-      return (
-        html = `
+      return (html = `
        <section class="foodContainer">
         <p class="icon">${food.emoji}</p>
         <div class="foodInformationContainer">
@@ -21,16 +21,16 @@ function renderMenu() {
         <p>${food.ingredients.join(", ")}</p>
         <p>$${food.price}</p>
         </div>
-        <div>
-        <button class="decrease-quantity" data-name="${food.name}" data-id="${
-        food.id
-      }">-</button>
+        <div class="quantity-container">
+        <button class="decrease-quantity btn" data-name="${
+          food.name
+        }" data-id="${food.id}">-</button>
         <p class="quantity" id="${food.name}-quantity" data-id="${food.id}">${
         quantity.item === undefined ? 0 : quantity.item
       }</p>
-        <button class="increase-quantity" data-name="${food.name}" data-id="${
-        food.id
-      }">+</button>
+        <button class="increase-quantity btn" data-name="${
+          food.name
+        }" data-id="${food.id}">+</button>
         </div>
         </section>
         `);
@@ -51,7 +51,6 @@ function renderEmptyCart() {
 // let quantity =
 // cartArray.find((cart) => cart.id === `${food.name}-quantity`) || [];
 
-
 function renderOrder() {
   const total = [];
   if (cartArray.length !== 0) {
@@ -60,29 +59,39 @@ function renderOrder() {
         let subtotal = Number(cart.price * cart.item);
         total.push(subtotal);
         return `<div class="order-item-container">
-      <p>${cart.emoji}</p>
+      <p class="icon">${cart.emoji}</p>
         <p>$${cart.price}</p>
-        <button class="decrease-quantity-cart" data-name="${cart.name}" data-id="${cart.id}">-</button>
-          <p class="quantity" id="${cart.name}-quantity" data-id="${cart.id}">${cart.item === undefined ? 0 : cart.item}</p>
-          <button class="increase-quantity-cart" data-name="${cart.name}" data-id="${cart.id}">+</button>
+        <div class="quantity-container">
+        <button class="decrease-quantity-cart btn" data-name="${
+          cart.name
+        }" data-id="${cart.id}">-</button>
+          <p class="quantity" id="${cart.name}-quantity" data-id="${cart.id}">${
+          cart.item === undefined ? 0 : cart.item
+        }</p>
+          <button class="increase-quantity-cart btn" data-name="${
+            cart.name
+          }" data-id="${cart.id}">+</button>
+          </div>
         <p>$${subtotal}</p>
-        <p id="${cart.name}-delete" class="delete-item" data-id=${cart.itemId}>X</p>
+        <button id="${cart.name}-delete" class="delete-item btn" data-id=${
+          cart.itemId
+        }>X</button>
         </div>`;
       })
       .join("");
     orderEl.innerHTML += `<div class="order-total-container">
-    <p>Total: $${total.reduce((a, b) => {
+    <p class="total">Total: $${total.reduce((a, b) => {
       return a + b;
-    }
-    )}
-    <button id ="checkout-btn" class="checkout-btn">Checkout</button>
+    })}</p>
+    <div class="checkout-container">
+    <button id ="checkout-btn" class="checkout-btn btn">Checkout</button>
     <button id="clear-cart-btn" class="clear-cart-btn btn">Clear Order</button>
+    </div>
     </div>`;
-  } 
-  else {
+  } else {
     renderEmptyCart();
   }
-  renderMenu()
+  renderMenu();
 }
 
 renderOrder();
@@ -90,97 +99,103 @@ renderOrder();
 function renderCheckout(e) {
   e.preventDefault();
   checkOutEl.classList.remove("hidden");
+  orderEl.classList.add("whiteout");
+  foodItem.classList.add("whiteout");
+  headerEl.classList.add("whiteout");
+  checkOutEl.classList.remove("whiteout");
   let createDiv = document.createElement("div");
-  const date = new Date()
-  let getMonth = date.getMonth() + 1
+  const date = new Date();
+  let getMonth = date.getMonth() + 1;
   let getYear = date.getFullYear();
- function month  () {
-  if(getMonth <10) {
-  return getMonth.toString().padStart(2, '0')
- }
- else {
-  return getMonth
- }}
-
- function year () {
-  if(getYear <= getYear) {
-  return getYear
- }
- else {
-  return 
- }}
+  function month() {
+    if (getMonth < 10) {
+      return getMonth.toString().padStart(2, "0");
+    } else {
+      return getMonth;
+    }
+  }
 
   createDiv.innerHTML = `
-  <h3>Enter card details</h3>
-  <p id="close-payment-btn" class="close-payment-btn">x</p>
-  <form>
-  <label for="name">Name</label>
-  <input type="text" id="name" name="name" placeholder="Name" required />
+  <section class="payment-container">
+  <div class="form-header">
+  <button id="close-payment-btn" class="close-checkout btn">x</button>
+  <h3 class="cc">Enter card details</h3>
+  </div>
+  <form class="form-container">
+  <label for="name">
+  <input type="text" id="name" name="name" placeholder="Full Name" pattern="[A-Za-z]{2,25}" required />
+  </label>
 
-  <label for="credit-card-number">Credit Card Number</label>
-  <input type="text" id="credit-card-number" name="credit-card-number" placeholder="Credit Card Number" required />
+  <label for="credit-card-number">
+  <input type="text" id="credit-card-number" name="credit-card-number" placeholder="Credit Card Number" pattern="[0-9]{3}" required /></label>
 
-  <label for="credit-card-date">Expiration Date</label>
-  <input type="month" id="credit-card-date" name="credit-card-date" min="${year()}-${month()}" value="${year()}-${month()}" required />
-  <label for="credit-card-code">Code</label>
-  <input type="text" id="credit-card-code" name="credit-card-code" placeholder="CSC code" minLength="3" maxLength="4" required />
-  <p id="missing"></p>
+  <label for="credit-card-date">
+  <input type="month" id="credit-card-date" name="credit-card-date" min="${getYear}-${month()}" value="${getYear}-${month()}" required /></label>
+
+  <label for="credit-card-code">
+  <input type="text" id="credit-card-code" name="credit-card-code" placeholder="CSC code" pattern="[0-9]{3}" required /></label>
+  <p id="missing" class="missing"></p>
 
   <button type="submit" id="payment-btn" class="payment-btn btn">Submit</button>
   </form>
-  `
+  </section>
+  `;
   return checkOutEl.appendChild(createDiv);
 }
 
-function paymentRequirements (e) {
-  const missingEl =document.getElementById("missing")
-  const nameEl = document.getElementById("name")
-  const creditCardNumEl = document.getElementById("credit-card-number")
-  const cvvEl = document.getElementById("credit-card-code")
-  if(nameEl.value.length > 2 && creditCardNumEl.value.length === 16 && cvvEl.value.length === 3) {
-    document.querySelector("body").innerHTML = `<h2>Thank you ${nameEl.value} for your order!</h2>
-                                                <p>It should ready in ${10 * cartArray.length} minutes.</p>
+function paymentRequirements(e) {
+  const missingEl = document.getElementById("missing");
+  const nameEl = document.getElementById("name");
+  const creditCardNumEl = document.getElementById("credit-card-number");
+  const cvvEl = document.getElementById("credit-card-code");
+  if (
+    nameEl.value.length > 2 &&
+    creditCardNumEl.value.length === 16 &&
+    cvvEl.value.length === 3
+  ) {
+    document.querySelector("body").innerHTML = `<h2 class="thank-you">Thank you ${
+      nameEl.value
+    } for your order!</h2>
+                                                <p>It should ready in ${
+                                                  10 * cartArray.length
+                                                } minutes.</p>
                                                 <p>The page will automatically go back to the ordering page in 10 seconds.</p>
-    `
+    `;
     setTimeout(() => location.reload(), 10000);
-  }
-  else if (nameEl.value.length < 2 ) {
-      if (creditCardNumEl.value.length < 16  || creditCardNumEl.value.length > 16) {
-        missingEl.innerHTML = ''
-        missingEl.innerHTML = `<p>Please enter your 16 digit credit card number and name with more than 2 letters</p>`
-      }
-
-      else if(cvvEl.value.length >3 || cvvEl.value.length < 3) {
-        missingEl.innerHTML = ''
-        missingEl.innerHTML = `<p>Please enter your 3 digit credit card cvv code and name with more than 2 letters</p>`
-      }
-    else{
-      missingEl.innerHTML = ''
-      missingEl.innerHTML = `<p>Please enter name with more than 2 letters</p>`
+  } else if (nameEl.value.length < 2) {
+    if (
+      creditCardNumEl.value.length < 16 ||
+      creditCardNumEl.value.length > 16
+    ) {
+      missingEl.innerHTML = "";
+      missingEl.innerHTML = `<p>Please enter your 16 digit credit card number and name with more than 2 letters</p>`;
+    } else if (cvvEl.value.length > 3 || cvvEl.value.length < 3) {
+      missingEl.innerHTML = "";
+      missingEl.innerHTML = `<p>Please enter your 3 digit credit card cvv code and name with more than 2 letters</p>`;
+    } else {
+      missingEl.innerHTML = "";
+      missingEl.innerHTML = `<p>Please enter name with more than 2 letters</p>`;
     }
-  }
-    else if  (creditCardNumEl.value.length < 16  || creditCardNumEl.value.length > 16) {
-        if(cvvEl.value.length >3 || cvvEl.value.length < 3) {
-          missingEl.innerHTML = ''
-          missingEl.innerHTML = `<p>Please enter your 16 digit credit card number and 3 digit credit card cvv code</p>`
-
-        }
-        else {
-          missingEl.innerHTML = ''
-          missingEl.innerHTML = '<p>Please enter your 16 digit credit card number</p>'
-        }
+  } else if (
+    creditCardNumEl.value.length < 16 ||
+    creditCardNumEl.value.length > 16
+  ) {
+    if (cvvEl.value.length > 3 || cvvEl.value.length < 3) {
+      missingEl.innerHTML = "";
+      missingEl.innerHTML = `<p>Please enter your 16 digit credit card number and 3 digit credit card cvv code</p>`;
+    } else {
+      missingEl.innerHTML = "";
+      missingEl.innerHTML =
+        "<p>Please enter your 16 digit credit card number</p>";
     }
-
-    else if(cvvEl.value.length >3 || cvvEl.value.length < 3){
-      missingEl.innerHTML = ''
-      missingEl.innerHTML = '<p>Please enter your 3 digit credit card cvv code</p>'
-    }
-  
-  else {
-    missingEl.innerHTML = ''
-    missingEl.innerHTML = `<p>Please enter all the information</p>`
+  } else if (cvvEl.value.length > 3 || cvvEl.value.length < 3) {
+    missingEl.innerHTML = "";
+    missingEl.innerHTML =
+      "<p>Please enter your 3 digit credit card cvv code</p>";
+  } else {
+    missingEl.innerHTML = "";
+    missingEl.innerHTML = `<p>Please enter all the information</p>`;
   }
-
 }
 
 function updateQuantity(item) {
@@ -191,25 +206,22 @@ function updateQuantity(item) {
 function cartQuantityIncrement(id) {
   let selectedItem = id;
   let quantity = cartArray.find((cart) => cart.id === selectedItem);
-  return quantity.item++
+  return quantity.item++;
 }
 
 function cartQuantityDecrement(id) {
   let selectedItem = id;
   let quantity = cartArray.find((cart) => cart.id === selectedItem);
-  if(quantity.item > 1){
-   quantity.item--
-   return cartArray
-}
-  else if(quantity.item === 1) {
-    console.log(quantity.item)
-    cartArray = cartArray.filter(cart => cart.id !== selectedItem)
-    return cartArray
+  if (quantity.item > 1) {
+    quantity.item--;
+    return cartArray;
+  } else if (quantity.item === 1) {
+    console.log(quantity.item);
+    cartArray = cartArray.filter((cart) => cart.id !== selectedItem);
+    return cartArray;
+  } else {
+    return;
   }
-  else {
-    return
-  }
-
 }
 
 function increment(id, item) {
@@ -261,7 +273,7 @@ function clearCart() {
   cartArray = [];
   renderMenu();
   renderOrder();
-  checkOutEl.innerHTML = ``
+  checkOutEl.innerHTML = ``;
 }
 
 document.addEventListener("click", (e) => {
@@ -296,7 +308,7 @@ document.addEventListener("click", (e) => {
       }
     });
   }
-  if(item[0] === "increase-quantity-cart") {
+  if (item[0] === "increase-quantity-cart") {
     cartArray.filter((item) => {
       item.quantity = document.getElementById(`${item.name}-quantity`);
       if (item.id.toString() === e.target.dataset.id) {
@@ -305,13 +317,13 @@ document.addEventListener("click", (e) => {
       }
     });
   }
-  if(item[0] === "decrease-quantity-cart") {
+  if (item[0] === "decrease-quantity-cart") {
     cartArray.filter((item) => {
       item.quantity = document.getElementById(`${item.name}-quantity`);
       if (item.id.toString() === e.target.dataset.id) {
         cartQuantityDecrement(item.quantity.id);
-        console.log(cartArray)
-        renderOrder()
+        console.log(cartArray);
+        renderOrder();
       }
     });
   }
@@ -319,14 +331,17 @@ document.addEventListener("click", (e) => {
     clearCart();
   }
 
-  if(item[0] === "checkout-btn") {
-    renderCheckout(e)
+  if (item[0] === "checkout-btn") {
+    renderCheckout(e);
   }
 
-  if(item[0] === "payment-btn") {
-    paymentRequirements(e)
+  if (item[0] === "payment-btn") {
+    paymentRequirements(e);
   }
-  if(item[0] === "close-payment-btn") {
-      checkOutEl.innerHTML = ``
+  if (item[0] === "close-checkout") {
+    checkOutEl.innerHTML = ``;
+    orderEl.classList.remove("whiteout");
+    foodItem.classList.remove("whiteout");
+    headerEl.classList.remove("whiteout");
   }
 });
